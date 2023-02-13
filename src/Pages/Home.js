374d2799c734e230681d6a71a78471b31c8b05db
Smarsden007 <seannmarsden@gmail.com>
2023-02-13 00:06:39 -0800

    Animated button added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,15 +5,20 @@ export const Home = () => {
   return (
     <div className="bg-[#F4ECD6]">
       <div className="flex flex-row justify-around ">
-        <h1 className="align-middle my-auto Lemon-Font w-1/5">
+        <div className="flex flex-col">
+        <h1 className="align-middle my-6 w-96 Lemon-Font">
           Don't let your event fall flat, make it pop with Whats Poppin Balloon
           Decor
-        </h1>
-        <img src={TanArch} className="h-full" />
-      </div>
-      <button className="border-solid border rounded shadow-xl  border-black text-[1.5rem] p-3 Retro-Font">
-        LEARN MORE
+        </h1> 
+        <button className="w-72">
+        <span class="button_top"> LEARN MORE</span>
       </button>
+      </div>
+      <div>
+        <img src={TanArch} className="h-full w-full " />
+        </div>
+      </div>
+     
     </div>
   );
 };
