3377fd96aeaa784f684d925918b1ca80d068c872
Smarsden007 <seannmarsden@gmail.com>
2023-02-13 00:00:57 -0800

    importing project fonts

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from './../Media/Logo.svg'
+import './../App.css'
 export const NavBar = () => {
   return (
     <nav className="flex items-center justify-between px-4 py-3 bg-[#F4ECD6]">
       <div className="flex items-center">
-        <img src={logo} alt="Logo" className="h-36" />
+        <img src={logo} alt="Logo" className="lg:h-36 lg:m-8" />
       </div>
       <div className="flex items-center">
-        <a className="px-3 py-2 rounded-lg text-white hover:bg-gray-700" href="/home">Home</a>
-        <a className="px-3 py-2 rounded-lg text-white hover:bg-gray-700" href="/gallery">Gallery</a>
-        <a className="px-3 py-2 rounded-lg text-white hover:bg-gray-700" href="/book">Book</a>
+        <a className="px-3 py-2 lg:m-4 rounded-lg text-black  Balloon-Font" href="/home">HOME</a>
+        <a className="px-3 py-2 lg:m-4 rounded-lg text-black  Balloon-Font" href="/gallery">GALLERY</a>
+        <a className="px-3 py-2 lg:m-4 rounded-lg text-black  Balloon-Font" href="/book">BOOK</a>
       </div>
     </nav>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import TanArch from "./../Media/TanArch.png";
+import './../App.css'
 export const Home = () => {
   return (
     <div className="bg-[#F4ECD6]">
-      <div className="flex flex-row justify-around align-middle">
-        <h1 className="align-middle my-auto">test</h1>
+      <div className="flex flex-row justify-around ">
+        <h1 className="align-middle my-auto Lemon-Font w-1/5">Don't let your event fall flat, make it pop with Whats Poppin Balloon Decor</h1>
         <img src={TanArch} className="h-full" />
-      </div>
+      </div><button className="border-solid border rounded shadow-xl  border-black text-[1.5rem] p-3 Retro-Font"> LEARN MORE </button>
     </div>
   );
 };
