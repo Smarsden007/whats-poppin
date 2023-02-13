4d43814487d8c46c7ecb21c2d14cfee1b8b34955
Smarsden007 <seannmarsden@gmail.com>
2023-02-13 00:01:31 -0800

    Clean up

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import TanArch from "./../Media/TanArch.png";
-import './../App.css'
+import "./../App.css";
 export const Home = () => {
   return (
     <div className="bg-[#F4ECD6]">
       <div className="flex flex-row justify-around ">
-        <h1 className="align-middle my-auto Lemon-Font w-1/5">Don't let your event fall flat, make it pop with Whats Poppin Balloon Decor</h1>
+        <h1 className="align-middle my-auto Lemon-Font w-1/5">
+          Don't let your event fall flat, make it pop with Whats Poppin Balloon
+          Decor
+        </h1>
         <img src={TanArch} className="h-full" />
-      </div><button className="border-solid border rounded shadow-xl  border-black text-[1.5rem] p-3 Retro-Font"> LEARN MORE </button>
+      </div>
+      <button className="border-solid border rounded shadow-xl  border-black text-[1.5rem] p-3 Retro-Font">
+        LEARN MORE
+      </button>
     </div>
   );
 };
