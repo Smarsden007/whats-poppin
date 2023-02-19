60c3138651243be35b780025b4b3d22ac7b20408
Smarsden007 <seannmarsden@gmail.com>
2023-02-18 22:46:07 -0800

    Re-arranged component heirarchy and working on subhero

---
+++ src/Components/Hero.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import TanArch from "./../Media/TanArch.png";
+
+export const Hero = () => {
+  return (
+
+         <div className="flex flex-row justify-around ">
+        <div className="flex flex-col">
+          <h1 className="align-middle my-6 w-96 Lemon-Font">
+            Don't let your event fall flat, make it pop with
+            <span> </span>
+            <span style={{color: "#ff0000" }}>W</span>
+            <span style={{ color: "#00ff00" }}>h</span>
+            <span style={{ color: "#0000ff" }}>a</span>
+            <span style={{ color: "#ffff00" }}>t</span>
+            <span style={{ color: "#00ffff" }}>s</span>
+            <span> </span>
+            <span style={{ color: "#ff00ff" }}>P</span>
+            <span style={{ color: "#0000ff" }}>o</span>
+            <span style={{ color: "#00ff00" }}>p</span>
+            <span style={{ color: "#ff0000" }}>p</span>
+            <span style={{ color: "#00ffff" }}>i</span>
+            <span style={{ color: "#ffff00" }}>n</span>
+            <span> </span>
+            Balloon Decor
+          </h1>
+
+          <button className="w-72">
+            <span class="button_top"> LEARN MORE</span>
+          </button>
+        </div>
+        <div>
+          <img alt='balloon arch' src={TanArch} className="h-full w-full " />
+        </div>
+      </div>
+  )
+}
---
+++ src/Components/SubHero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./../App.css";
+
+export const SubHero = () => {
+  return (
+    <div>
+      <div className="flex flex-row justify-around">
+        <h2 className="Balloon-Font transform -rotate-90">LETS GET POPPIN</h2>
+        <div className="border-4 border-black w-8/12 h-96">
+
+        </div>
+      </div>
+    </div>
+  );
+};
