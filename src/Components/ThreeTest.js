edb71d8cb2bcdae70525db84c34492a6ddc3ffea
Smarsden007 <seannmarsden@gmail.com>
2023-03-15 22:53:33 -0700

    WIP

--- src/Components/ThreeTest.js
+++ src/Components/ThreeTest.js
@@ -1,37 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function ThreeTest() {
+  const rendererRef = useRef(null);
+  const sceneRef = useRef(null);
+  const cameraRef = useRef(null);
+
   useEffect(() => {
     // create a scene
     const scene = new THREE.Scene();
+    sceneRef.current = scene;
 
     // create a camera
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
+    cameraRef.current = camera;
 
     // create a renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
-
-    // create a cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    rendererRef.current.appendChild(renderer.domElement);
+    // load the 3D object
+    const loader = new GLTFLoader();
+    loader.load('./../Media/untitled.glb', (gltf) => {
+      const object = gltf.scene;
+      scene.add(object);
+    });
 
     // render the scene
     function animate() {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
       renderer.render(scene, camera);
     }
     animate();
   }, []);
 
-  return <div />;
+  return <div ref={rendererRef} />;
 }
 
 export default ThreeTest;
