10aab7dea3d3b474ace5d96c5bc9677540f25e75
Smarsden007 <seannmarsden@gmail.com>
2023-03-15 22:43:50 -0700

    Adding a test three.js scene

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,14 @@ import "./../App.css";
 import { Hero } from "../Components/Hero";
 import { SubHero } from "../Components/SubHero";
 import { BalloonBox } from "../Components/BalloonBox";
+import ThreeTest from "../Components/ThreeTest";
 
 
 export const Home = () => {
   return (
     <div className="bg-[#F4ECD6]">
      <Hero />
+     <ThreeTest />
      <SubHero />
      <BalloonBox />
     </div>
