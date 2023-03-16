c23080529feb6492f8680beb57343cc87829c815
Smarsden007 <seannmarsden@gmail.com>
2023-03-15 22:36:19 -0700

    Minor changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,49 +2,48 @@ import React from 'react'
 import tiktok from './../Media/TikTok.png'
 import facebook from './../Media/Facebook.png'
 import instagram from './../Media/Instagram.png'
+import { Link } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <footer className="bg-[#F4ECD6] text-gray-500">
+    <footer className="bg-[#F4ECD6] text-black-500">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-          © 2022 Whats Poppin Sonoma —
-          <a
-            href="https://twitter.com/intent/tweet?url=#"
-            className="text-gray-600 ml-1"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @whatspoppinsonoma
-          </a>
+        <p className="text-sm text-black-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
+          © 2022 Whats Poppin Sonoma
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500">
-            <img src={tiktok} alt='tiktok' />
+          <a className="text-gray-500" href="https://www.tiktok.com/">
+            <img src={tiktok} alt='tiktok' className='hover:scale-110' />
           </a>
-          <a className="ml-4 text-gray-500">
-          <img src={facebook} alt='tiktok' />
+          <a className="ml-4 text-gray-500" href="https://www.tiktok.com/">
+          <img src={facebook} alt='tiktok' className='hover:scale-110' />
 
           </a>
-          <a className="ml-4 text-gray-500">
-          <img src={instagram} alt='tiktok' />
+          <a className="ml-4 text-gray-500" href="https://www.tiktok.com/">
+          <img src={instagram} alt='tiktok' className='hover:scale-110' />
 
           </a>
+          
         </span>
+        
         <nav className="sm:ml-auto sm:mt-0 mt-4 flex flex-wrap items-center text-base justify-center">
+          <Link to='/home'>
           <a href="/" className="mr-5 hover:text-gray-300">
             Home
           </a>
-          <a href="/about" className="mr-5 hover:text-gray-300">
-            About
+          </Link>
+          <Link to='/gallery'>
+          <a href="/gallery" className="mr-5 hover:text-gray-300">
+            Gallery
           </a>
-          <a href="/contact" className="hover:text-gray-300">
-            Contact
+          </Link>
+          <Link to='/book'>
+          <a href="/book" className="hover:text-gray-300">
+            Book
           </a>
+          </Link>
         </nav>
-        <p className="sm:mt-0 mt-4 text-gray-500">
-          Phone: (555) 555-5555
-        </p>
+       
       </div>
     </footer>
   )
--- src/Components/SubHero.js
+++ src/Components/SubHero.js
@@ -3,13 +3,13 @@ import "./../App.css";
 
 export const SubHero = () => {
   return (
-    <div className="m-8 p-8">
+    <div className="flex flex-col justify-center align-center items-center m-8 p-8">
       <div className="flex lg:flex-row flex-col justify-center gap-0">
         <div className="border-4 border-black lg:border-r-2 lg:w-3/12 p-4">
           <h3 className="text-center p-4 Lemon-Font">Step 1.</h3>
           <h4 className="text-center p-2 Lemon-Font2 text-[1rem2]">
             Browse collection of luxury balloon arrangements. Select the
-            arrangement that best suits your needs and preferences.Choose your
+            arrangement that best suits your needs and preferences. Choose your
             preferred color scheme, theme, and any additional customizations you
             would like to add.
           </h4>
@@ -19,7 +19,7 @@ export const SubHero = () => {
           <h4 className="text-center p-2 Lemon-Font2 text-[1rem2]">
             Specify any special requests or instructions you may have for the
             arrangement. Provide your contact and delivery information,
-            including your name, address, phone number, and email address.Select
+            including your name, address, phone number, and email address. Select
             your preferred delivery date and time.
           </h4>
         </div>
@@ -32,6 +32,9 @@ export const SubHero = () => {
           </h4>
         </div>
       </div>
+      <button className="w-48 m-6">
+            <span class="button_top "> BOOK NOW</span>
+          </button>
     </div>
   );
 };
